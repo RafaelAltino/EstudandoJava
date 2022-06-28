29ca155582cc43076849b120d5beda940be49538
Rafael Felipe Reis Altino <rafaelfraltino@gmail.com>
2022-06-27 22:44:40 -0300

    Primeiro desafio

--- DESAFIOS/src/App.java
+++ DESAFIOS/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("frdts2XXX6xxbl2XXXeee5: ");
+        System.out.println(Exercicio.calcula("frdts2XXX6xxbl2XXXeee5")); 
+
+        System.out.print("frdts2XXX1xxbl2XXXeee5: ");
+        System.out.println(Exercicio.calcula("frdts2XXX1xxbl2XXXeee5")); 
+
+        System.out.print("frdts2XXX1xxbl4XXX4eee5: ");
+        System.out.println(Exercicio.calcula("frdts2XXX1xxbl4XXX4eee5")); 
     }
 }

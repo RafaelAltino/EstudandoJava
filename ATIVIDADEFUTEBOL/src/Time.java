bd860bf97e06ec729688181eeed2d478f9b170ad
F8367123 Rafael Felipe Reis Altino <rafael.altino@bb.com.br>
2022-07-20 15:16:44 -0300

    Finalizando atividade futebol

---
+++ ATIVIDADEFUTEBOL/src/Time.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class Time {
+    private String nome;
+    private List<Jogador> jogadores;
+
+    public Time(String nome, List<Jogador> jogadores){
+        this.nome = nome;
+        this.jogadores = jogadores;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Jogador> getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(List<Jogador> jogadores) {
+        this.jogadores = jogadores;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

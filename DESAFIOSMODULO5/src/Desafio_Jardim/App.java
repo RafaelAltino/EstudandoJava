51e42581892742985806c45cece832fa703c790e
Rafael Felipe Reis Altino <rafaelfraltino@gmail.com>
2022-07-24 16:55:31 -0300

    Fazendo desafio jardim em casa

--- DESAFIOSMODULO5/src/Desafio_Jardim/App.java
+++ DESAFIOSMODULO5/src/Desafio_Jardim/App.java
@@ -1,5 +1,7 @@
 package Desafio_Jardim;
 
+import java.text.Collator;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -7,27 +9,54 @@ import java.util.stream.Stream;
 import org.w3c.dom.ranges.Range;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         
-        Stream<Integer> jardim = Stream.generate( () -> 1)
-            .limit(100);
-        
-            Stream jardim2 = Stream.of(
+        Supplier<Stream<Integer>> jardim = () -> {
+            var jardim1 = Stream.of(
                 Stream.generate( () -> 1)
-                .limit(100).collect(Collectors.toList()),
+                .limit(100).collect(Collectors.toList()),                
                 Stream.generate( () -> 2)
                 .limit(50).collect(Collectors.toList()),
                 Stream.generate( () -> 3)
                 .limit(25).collect(Collectors.toList())
-            );
+            ).flatMap( e -> e.stream());
+            return jardim1;
+        };
 
+        // Stream<Integer> jardim = Stream.of(
+        //     Stream.generate( () -> 1)
+        //     .limit(100).collect(Collectors.toList()),               
+        //     Stream.generate( () -> 2)
+        //     .limit(50).collect(Collectors.toList()),
+        //     Stream.generate( () -> 3)
+        //     .limit(25).collect(Collectors.toList())
+        // ).flatMap( e -> e.stream());
             
+
+        System.out.println("Jardim inicial:");
+
         
-        jardim2
-        .forEach( System.out::println)
+
+        jardim.get()
+            .map( (n) -> n + ", ")
+            .forEach(System.out::print);
+
+        System.out.println("\nJardim Bagunçado:");
+
+        jardim.get()
+            .unordered()
+            .parallel()
+            .map((n) -> n +", ")           
+            .forEach(System.out::print);
+        
+        System.out.println("\nApós colheita:");
+        jardim.get()
+        .unordered()
+        .parallel()
+        .filter( (n) -> n != 3)
+        .skip((long) (jardim.get().toList().size() * 0.25))
+        .map((n) -> n +", ")
+        .forEach(System.out::print);
         ;
-
-
-        
     }
 }
